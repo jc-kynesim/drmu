1592cd12c5a84b38e5c7a62faa0c477ebea27f27
John Cox <jc@kynesim.co.uk>
2023-02-01 11:58:45 +0000

    drmu_vlc: Add some more formats

--- drmu_vlc.c
+++ drmu_vlc.c
@@ -100,8 +100,27 @@ drmu_format_vlc_chroma_to_drm(const vlc_fourcc_t chroma)
             return DRM_FORMAT_NV12;
         case VLC_CODEC_NV21:
             return DRM_FORMAT_NV21;
+        case VLC_CODEC_NV16:
+            return DRM_FORMAT_NV16;
+        case VLC_CODEC_NV61:
+            return DRM_FORMAT_NV61;
+        case VLC_CODEC_NV24:
+            return DRM_FORMAT_NV24;
+        case VLC_CODEC_NV42:
+            return DRM_FORMAT_NV42;
+        case VLC_CODEC_P010:
+            return DRM_FORMAT_P010;
+        case VLC_CODEC_J420:
         case VLC_CODEC_I420:
             return DRM_FORMAT_YUV420;
+        case VLC_CODEC_YV12:
+            return DRM_FORMAT_YVU420;
+        case VLC_CODEC_J422:
+        case VLC_CODEC_I422:
+            return DRM_FORMAT_YUV422;
+        case VLC_CODEC_J444:
+        case VLC_CODEC_I444:
+            return DRM_FORMAT_YUV444;
         default:
             break;
     }
@@ -133,6 +152,18 @@ drmu_format_vlc_to_drm(const video_frame_format_t * const vf_vlc)
                 return DRM_FORMAT_BGRX8888;
             break;
         }
+        case VLC_CODEC_RGB24:
+        {
+            // VLC RGB24 aka RV24 means we have to look at the mask values
+            const uint32_t r = vf_vlc->i_rmask;
+            const uint32_t g = vf_vlc->i_gmask;
+            const uint32_t b = vf_vlc->i_bmask;
+            if (r == 0xff0000 && g == 0xff00 && b == 0xff)
+                return DRM_FORMAT_RGB888;
+            if (r == 0xff && g == 0xff00 && b == 0xff0000)
+                return DRM_FORMAT_BGR888;
+            break;
+        }
         case VLC_CODEC_RGB16:
         {
             // VLC RGB16 aka RV16 means we have to look at the mask values
@@ -161,6 +192,9 @@ drmu_format_vlc_to_vlc(const uint32_t vf_drm)
         case DRM_FORMAT_RGBX8888:
         case DRM_FORMAT_BGRX8888:
             return VLC_CODEC_RGB32;
+        case DRM_FORMAT_RGB888:
+        case DRM_FORMAT_BGR888:
+            return VLC_CODEC_RGB24;
         case DRM_FORMAT_BGR565:
         case DRM_FORMAT_RGB565:
             return VLC_CODEC_RGB16;
@@ -185,8 +219,24 @@ drmu_format_vlc_to_vlc(const uint32_t vf_drm)
             return VLC_CODEC_NV12;
         case DRM_FORMAT_NV21:
             return VLC_CODEC_NV21;
+        case DRM_FORMAT_NV16:
+            return VLC_CODEC_NV16;
+        case DRM_FORMAT_NV61:
+            return VLC_CODEC_NV61;
+        case DRM_FORMAT_NV24:
+            return VLC_CODEC_NV24;
+        case DRM_FORMAT_NV42:
+            return VLC_CODEC_NV42;
+        case DRM_FORMAT_P010:
+            return VLC_CODEC_P010;
         case DRM_FORMAT_YUV420:
             return VLC_CODEC_I420;
+        case DRM_FORMAT_YVU420:
+            return VLC_CODEC_YV12;
+        case DRM_FORMAT_YUV422:
+            return VLC_CODEC_I422;
+        case DRM_FORMAT_YUV444:
+            return VLC_CODEC_I444;
         default:
             break;
     }
