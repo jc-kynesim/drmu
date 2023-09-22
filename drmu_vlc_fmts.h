4da1d9e0d5d22a5e48bd7f76218ea3a36c701909
John Cox <jc@kynesim.co.uk>
2023-09-22 16:16:21 +0000

    drmu_vlc_fmts: Rework to be table based and use drm modifiers

--- drmu_vlc_fmts.h
+++ drmu_vlc_fmts.h
@@ -8,25 +8,40 @@
 #endif
 #define HAS_DRMPRIME 1
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <vlc_common.h>
-#include <vlc_picture.h>
+//#include <vlc_picture.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct drmu_vlc_fmt_info_ss;
+typedef struct drmu_vlc_fmt_info_ss drmu_vlc_fmt_info_t;
+
+const drmu_vlc_fmt_info_t * drmu_vlc_fmt_info_find_vlc(const video_frame_format_t * const vf_vlc);
+const drmu_vlc_fmt_info_t * drmu_vlc_fmt_info_find_drm(const uint32_t pixelformat, const uint64_t modifier);
+
+vlc_fourcc_t drmu_vlc_fmt_info_vlc_chroma(const drmu_vlc_fmt_info_t * const f);
+uint32_t drmu_vlc_fmt_info_drm_pixelformat(const drmu_vlc_fmt_info_t * const f);
+uint64_t drmu_vlc_fmt_info_drm_modifier(const drmu_vlc_fmt_info_t * const f);
+
+bool drmu_vlc_fmt_info_is_zc_cma(const drmu_vlc_fmt_info_t * const f);
+bool drmu_vlc_fmt_info_is_drmprime(const drmu_vlc_fmt_info_t * const f);
+
+
 // Convert chroma to drm - can't cope with RGB32 or RGB16 as they require
 // more info. returns 0 if unknown.
 uint32_t drmu_format_vlc_chroma_to_drm(const vlc_fourcc_t chroma);
 // Convert format to drm fourcc - does cope with RGB32 & RGB16
-uint32_t drmu_format_vlc_to_drm(const video_frame_format_t * const vf_vlc);
-vlc_fourcc_t drmu_format_vlc_to_vlc(const uint32_t vf_drm);
+// pMod receives modifier - may be null
+uint32_t drmu_format_vlc_to_drm(const video_frame_format_t * const vf_vlc, uint64_t * const pMod);
 
 #if HAS_DRMPRIME
 // pmod may be NULL
-uint32_t drmu_format_vlc_to_drm_prime(const vlc_fourcc_t chroma_in, uint64_t * const pmod);
+uint32_t drmu_format_vlc_to_drm_prime(const video_frame_format_t * const vf_vlc, uint64_t * const pmod);
 #endif
 #if HAS_ZC_CMA
 uint32_t drmu_format_vlc_to_drm_cma(const vlc_fourcc_t chroma_in);
