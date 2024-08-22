4c6916981f7e80e7ef5db2f7895573be03f3cc89
John Cox <jc@kynesim.co.uk>
2024-08-22 15:56:41 +0100

    drmu_poll: Separate out queued commit code from the core
    Separate out the queued commit code.
    Only start the poll thread once it is needed on the first call to
    drmu_atomic_queue. This allows interoperation with other code that needs
    an event queue where the drmu-based code doesn't and means we don't have
    a spare thread hanging around if we don't need one.

---
+++ drmu/drmu_poll.h
@@ -0,0 +1,27 @@
+#ifndef _DRMU_DRMU_POLL_H
+#define _DRMU_DRMU_POLL_H
+
+struct drmu_env_s;
+struct drmu_atomic_s;
+
+// Q the atomic on its associated env
+//
+// in-progress = The commit has been done but no ack yet
+// pending     = Commit Qed to be done when the in-progress commit has
+//               completed
+//
+// If no in-progress commit then this will be committed immediately
+// otherwise it becomes the pending commit
+// If there is a pending commit this atomic will be merged with it
+// Commits are done with the PAGE_FLIP flag set so we expect the ack
+// on the next page flip.
+int drmu_atomic_queue(struct drmu_atomic_s ** ppda);
+// Wait for there to be no pending commit (there may be a commit in
+// progress)
+int drmu_env_queue_wait(struct drmu_env_s * const du);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
