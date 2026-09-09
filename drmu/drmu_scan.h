88b5064b06607ae01f8ddef6889ef50bfc99aebf
John Cox <jc@kynesim.co.uk>
2026-09-09 18:34:05 +0100

    Stash

--- drmu/drmu_scan.h
+++ drmu/drmu_scan.h
@@ -9,8 +9,11 @@ struct drmu_log_env_s;
 struct drmu_env_s;
 struct drmu_output_s;
 
+// Scan /dev/drv/card* in numeric order looking for a device with the given conn_name
+// as a connected connector. conn_name is prefix matched only so "DP" will likely match
+// the first connected display port and "" will find the first connected anything.
 int
-drmu_scan_output(const char * const cname, const struct drmu_log_env_s * const dlog,
+drmu_scan_output(const char * const conn_name, const struct drmu_log_env_s * const dlog,
                  struct drmu_env_s ** const pDu, struct drmu_output_s ** const pDoutput);
 
 #ifdef __cplusplus
