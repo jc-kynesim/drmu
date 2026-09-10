496a33bc7ccf9cf2f632c81263608d5c6c57f9b5
John Cox <jc@kynesim.co.uk>
2026-09-10 17:54:12 +0100

    drmu_scan: Rework scan s.t. it actually scans the dri directory
    Previous version just tried card0 to n till it failed to find one.
    This didn't work on ubuntu which started its card numbers at 1. So we
    now have a proper scanner that reads the directory.
    
    Exposes the card scanner iterator for use by other progs too.
    
    There is still room for improvement when looking for the best device
    across multiple devices

--- drmu/drmu_scan.c
+++ drmu/drmu_scan.c
@@ -1,60 +1,274 @@
 #include "drmu_scan.h"
 
+#include <assert.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
+#include <stdatomic.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "drmu.h"
 #include "drmu_log.h"
 #include "drmu_output.h"
 
-#define CARD_MAX 16
+#define CARD_DIR    "/dev/dri"
+#define CARD_PREFIX "card"
 
+typedef struct card_list_s {
+    unsigned int size;
+    unsigned int n;
+    unsigned int * nos;
+} card_list_t;
+
+struct drmu_scan_s {
+    atomic_int ref_count;  // 0 == 1 ref for ease of init
+    const drmu_log_env_t * dlog;
+    card_list_t cl;
+    unsigned int cur_idx;
+    char pathname[64];
+};
+
+static void
+card_list_uninit(card_list_t * const cl)
+{
+    free(cl->nos);
+    cl->nos = NULL;
+    cl->size = 0;
+    cl->n = 0;
+}
+
+static int
+card_list_add(card_list_t * const cl, const unsigned int cardno)
+{
+    if (cl->n >= cl->size) {
+        const unsigned int newsize = cl->size < 16 ? 16 : cl->size * 2;
+        unsigned int * const newnos = realloc(cl->nos, newsize * sizeof(*newnos));
+
+        if (newnos == NULL)
+            return -ENOMEM;
+        cl->nos = newnos;
+        cl->size = newsize;
+    }
+    cl->nos[cl->n++] = cardno;
+    return 0;
+}
+
+static int
+card_no_qsort_cb(const void * va, const void * vb)
+{
+    const unsigned int a = *(const unsigned int *)va;
+    const unsigned int b = *(const unsigned int *)vb;
+
+    return a < b ? -1 : a > b ? 1 : 0;
+}
+
+// Match "card<n>" exactly - returns -1 if no match
+static int
+card_no_parse(const char * name)
+{
+    unsigned long n;
+    char * endp;
+
+    if (strncmp(name, CARD_PREFIX, sizeof(CARD_PREFIX) - 1) != 0)
+        return -1;
+    name += sizeof(CARD_PREFIX) - 1;
+    if (*name < '0' || *name > '9')
+        return -1;
+
+    n = strtoul(name, &endp, 10);
+    if (*endp != '\0' || n > INT_MAX)
+        return -1;
+
+    return (int)n;
+}
+
+// Build a list of card numbers found in the dri dir, sorted ascending
+static int
+card_list_scan(card_list_t * const cl, const drmu_log_env_t * const dlog)
+{
+    DIR * dir;
+    const struct dirent * de;
+    int rv = 0;
+
+    if ((dir = opendir(CARD_DIR)) == NULL) {
+        rv = -errno;
+        drmu_err_log(dlog, "Failed to open " CARD_DIR ": %s", strerror(-rv));
+        return rv;
+    }
+
+    while ((de = readdir(dir)) != NULL) {
+        const int cardno = card_no_parse(de->d_name);
+
+        if (cardno >= 0 &&
+            (rv = card_list_add(cl, cardno)) != 0) {
+            drmu_err_log(dlog, "Failed to add card %d to list: %s", cardno, strerror(-rv));
+            break;
+        }
+    }
+
+    closedir(dir);
+
+    if (rv != 0)
+        return rv;
+
+    qsort(cl->nos, cl->n, sizeof(*cl->nos), card_no_qsort_cb);
+    return 0;
+}
+
+static void
+scan_free(drmu_scan_t * const dscan)
+{
+    card_list_uninit(&dscan->cl);
+    free(dscan);
+}
+
+void
+drmu_scan_unref(drmu_scan_t ** const ppdscan)
+{
+    drmu_scan_t *const dscan = *ppdscan;
+    int n;
+
+    if (dscan == NULL)
+        return;
+    *ppdscan = NULL;
+
+    n = atomic_fetch_sub(&dscan->ref_count, 1);
+    assert(n >= 0);
+    if (n != 0)
+        return;
+
+    scan_free(dscan);
+}
+
+drmu_scan_t *
+drmu_scan_ref(drmu_scan_t * const dscan)
+{
+    if (dscan != NULL)
+        atomic_fetch_add(&dscan->ref_count, 1);
+    return dscan;
+}
+
+static bool
+scan_eol(const drmu_scan_t * const dscan)
+{
+    return dscan == NULL || dscan->cur_idx >= dscan->cl.n;
+}
+
+const char *
+drmu_scan_path(drmu_scan_t * const dscan)
+{
+    if (scan_eol(dscan))
+        return NULL;
+
+    snprintf(dscan->pathname, sizeof(dscan->pathname), CARD_DIR "/" CARD_PREFIX "%u", dscan->cl.nos[dscan->cur_idx]);
+    return dscan->pathname;
+}
 
 int
-drmu_scan_output(const char * const cname, const drmu_log_env_t * const dlog,
+drmu_scan_next(drmu_scan_t * const dscan)
+{
+    if (scan_eol(dscan))
+        return -ENOENT;
+
+    ++dscan->cur_idx;
+    return dscan->cur_idx >= dscan->cl.n ? -ENOENT : 0;
+}
+
+struct drmu_env_s *
+drmu_scan_du(drmu_scan_t * const dscan)
+{
+    const char * path;
+
+    for (; (path = drmu_scan_path(dscan)) != NULL; drmu_scan_next(dscan)) {
+        drmu_env_t * du;
+        int fd;
+
+        drmu_debug_log(dscan->dlog, "Try card %s", path);
+
+        while ((fd = open(path, O_RDWR | O_CLOEXEC)) == -1 && errno == EINTR)
+            /* Loop */;
+        if (fd == -1)
+            continue;
+
+        // Have FD
+        if ((du = drmu_env_new_fd(fd, dscan->dlog)) != NULL)
+            return du;
+
+        drmu_debug_log(dscan->dlog, "Cannot create du");
+    }
+
+    return NULL;
+}
+
+drmu_scan_t *
+drmu_scan_new(const drmu_log_env_t * const dlog)
+{
+    drmu_scan_t * const dscan = calloc(1, sizeof(*dscan));
+
+    if (dscan == NULL)
+        return NULL;
+
+    atomic_init(&dscan->ref_count, 0);
+    dscan->dlog = dlog == NULL ? &drmu_log_env_none : dlog;
+
+    if (card_list_scan(&dscan->cl, dscan->dlog) != 0)
+        goto fail;
+
+    return dscan;
+
+fail:
+    scan_free(dscan);
+    return NULL;
+}
+
+int
+drmu_scan_output(const char * const conn_name, const drmu_log_env_t * const dlog,
                  drmu_env_t ** const pDu, drmu_output_t ** const pDoutput)
 {
-    static const char * card_prefix = "/dev/dri/card";
-    unsigned int i;
+    drmu_scan_t * dscan;
+    drmu_env_t * du;
+    drmu_output_t * dout_best = NULL;
+    int score_best = 0;
 
     *pDu = NULL;
     *pDoutput = NULL;
 
-    for (i = 0; i != CARD_MAX; ++i) {
-        drmu_env_t * du;
+    for (dscan = drmu_scan_new(dlog); (du = drmu_scan_du(dscan)) != NULL; drmu_scan_next(dscan)) {
         drmu_output_t * dout = NULL;
-        char fname[32];
-        int fd;
+        int score;
 
-        drmu_debug_log(dlog, "Try card %d", i);
-
-        sprintf(fname, "%s%d", card_prefix, i);
-        while ((fd = open(fname, O_RDWR | O_CLOEXEC)) == -1 && errno == EINTR)
-            /* Loop */;
-        if (fd == -1) {
-            if (errno == ENOENT)
-                break;
-            continue;
+        if ((dout = drmu_output_new(du)) == NULL) {
+            drmu_debug(du, "Cannot create output");
         }
-
-        // Have FD
-        if ((du = drmu_env_new_fd(fd, dlog)) == NULL)
-            continue;
-
-        if ((dout = drmu_output_new(du)) == NULL)
-            goto loop1;
-
-        if (drmu_output_add_output(dout, cname) == 0) {
-            *pDu = du;
-            *pDoutput = dout;
-            return 0;
+        else if ((score = drmu_output_add_output2(dout, conn_name,
+                                                  DRMU_OUTPUT_FLAG_ADD_SCORE_GT(score_best))) < 0) {
+            drmu_debug(du, "Could not add output for conn '%s'", conn_name);
+        }
+        else {
+            drmu_debug(du, "Added output OK");
+            drmu_output_unref(&dout_best);
+            dout_best = drmu_output_ref(dout);
+            score_best = score;
         }
 
         drmu_output_unref(&dout);
-loop1:
         drmu_env_unref(&du);
+
+        // We aren't going to do better than this - might as well stop now
+        if (score == DRMU_OUTPUT_ADD_SCORE_MAX)
+            break;
     }
-    return -ENOENT;
+
+    drmu_scan_unref(&dscan);
+
+    if (dout_best == NULL)
+        return -ENOENT;
+
+    *pDu = drmu_env_ref(drmu_output_env(dout_best));
+    *pDoutput = dout_best;
+    return 0;
 }
 
