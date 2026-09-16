df01c292ec6ea180ae1709a0cb499e002fa671ad
John Cox <jc@kynesim.co.uk>
2026-09-16 15:21:35 +0100

    10bittest: Trivial whitespace fix

--- test/10bittest.c
+++ test/10bittest.c
@@ -978,8 +978,8 @@ int main(int argc, char *argv[])
                 }
                 break;
             case 'C':
-                    conn_name = optarg;
-                    break;
+                conn_name = optarg;
+                break;
             case 'c':
                 colorspace = optarg;
                 break;
